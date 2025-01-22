aeae4c7b7d0548a66cf13885b9d3103367f4e889
andrei moskalev <moskalev.web.dev@gmail.com>
2025-01-22 06:03:56 +0300

    13. Шапка страниц. Функционал

--- src/components/header/components/control-panel/control-panel.js
+++ src/components/header/components/control-panel/control-panel.js
@@ -1,5 +1,6 @@
-import { Icon } from "../../../../components";
+import { Link, useNavigate } from "react-router-dom";
 
+import { Icon } from "../../../../components";
 import styled from "styled-components";
 
 const RightAligned = styled.div`
@@ -7,22 +8,41 @@ const RightAligned = styled.div`
 	justify-content: flex-end;
 `;
 
-const Button = styled.button`
+const StyledLink = styled(Link)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	font-size: 18px;
 	width: 100px;
 	height: 32px;
+	border: 1px solid #000;
+	background: #eee;
+`;
+
+const StyledButton = styled.div`
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 const ControlPanelContainer = ({ className }) => {
+	const navigate = useNavigate();
 	return (
 		<div className={className}>
 			<RightAligned>
-				<Button>Войти</Button>
+				<StyledLink to="/login">Войти</StyledLink>
 			</RightAligned>
 			<RightAligned>
-				<Icon id="fa-backward" margin="10px 0 0 0" />
-				<Icon id="fa-file-text-o" margin="10px 0 0 16px" />
-				<Icon id="fa-users" margin="10px 0 0 16px" />
+				<StyledButton onClick={() => navigate(-1)}>
+					<Icon id="fa-backward" margin="10px 0 0 0" />
+				</StyledButton>
+				<Link to="/post">
+					<Icon id="fa-file-text-o" margin="10px 0 0 16px" />
+				</Link>
+
+				<Link to="/users">
+					<Icon id="fa-users" margin="10px 0 0 16px" />
+				</Link>
 			</RightAligned>
 		</div>
 	);
--- src/components/header/components/logo/logo.js
+++ src/components/header/components/logo/logo.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Icon } from "../../../../components";
 import styled from "styled-components";
 
@@ -13,16 +14,16 @@ const SmallText = styled.div`
 	font-weight: bold;
 `;
 const LogoContainer = ({ className }) => (
-	<div className={className}>
+	<Link className={className} to="/">
 		<Icon id="fa-code" size="70px" margin="0 10px 0 0" />
 		<div>
 			<LargeText>Блог</LargeText>
 			<SmallText>веб-разработчика</SmallText>
 		</div>
-	</div>
+	</Link>
 );
 
 export const Logo = styled(LogoContainer)`
 	display: flex;
-	margin-top: -14px;
+	margin-top: -21px;
 `;
