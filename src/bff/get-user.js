e5b394ddc3089a74a3b2747b284b98087125c4a9
andrei moskalev <moskalev.web.dev@gmail.com>
2025-02-04 01:06:51 +0300

    fix log users

--- src/bff/get-user.js
+++ src/bff/get-user.js
@@ -4,31 +4,7 @@ export const getUser = async (loginToFind) => {
 	);
 	const users = await response.json(); // JSON Server возвращает массив пользователей
 
-	console.log("Найденные пользователи:", users);
+	// console.log("Найденные пользователи:", users);
 
 	return users[0]; // Берём первого пользователя (если он есть)
 };
-
-const user = await getUser("testUser");
-console.log(user);
-
-// старый код
-// export const getUser = async (loginToFind) => {
-// 	fetch(`http://localhost:3005/users?login=${loginToFind}`)
-// 		.then((loadedUser) => loadedUser.json())
-// 		.then(([loadedUser]) => loadedUser);
-// };
-
-// export const getUser = async (loginToFind) => {
-// 	const response = await fetch(`http://localhost:3005/users`);
-// 	const users = await response.json(); // Загружаем всех
-
-// 	// Ищем пользователя без учёта регистра
-// 	const user = users.find(
-// 		(u) => u.login.toLowerCase() === loginToFind.toLowerCase(),
-// 	);
-
-// 	console.log("Найден пользователь:", user);
-
-// 	return user; // Вернёт объект или undefined
-// };
